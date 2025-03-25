84d1ce641685a7b10026b49de55b20223388cf54
Matias Navio <naviomatias2@gmail.com>
2025-03-25 10:38:13 -0300

    Objetos en parámetros de funciones

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -10,6 +10,8 @@ const addNumberArrow = (a:number, b:number):string => {
     return `${a + b}`;
 }
 
+// funcion que tiene 3 parametros, uno obligatorio, uno opcional y otro ya definido.
+// Realiza multiplicacion dependiendo de los parametros
 function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
 
     if(secondNumber != null){
@@ -21,13 +23,40 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
 
 // le declaramos como valor de result, lo que retorna la funcion
 const result: number = addNumbers(2, 2);
-
 // llamamos a la funcion flecha
 const result2: string = addNumberArrow(2, 2);
-
+// llamamos a la funcion multiply
 const multiplyReuslt: number = multiply(5, 5);
 
 // devolvemos le resultado com objeto
 console.log({result, result2, multiplyReuslt});
+
+// definimos una interface para un objeto Character
+interface Character {
+    name: string,
+    hp: number;
+    // manera de definir una funcion dentro de una interface
+    showHp: () => void;
+}
+
+// funcion que le cura hp al Character
+const healCharacter = (character: Character, amount:number) => {
+    console.log('curando...')
+    character.hp += amount;
+}
+
+// objeto strider en base a la interface
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+// mostramos la vida, curamos con la funcion y volvemos a mostrar
+strider.showHp();
+healCharacter(strider, 50);
+strider.showHp(); 
 
 export{};
