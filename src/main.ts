5085a0c288c396ec0efaed4b0e2fc9acc6a5040a
Matias Navio <naviomatias2@gmail.com>
2025-03-25 08:05:30 -0300

    Objetos e interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './topics/01-basic-types';
+// import './topics/01-basic-types';
+import './topics/02-objects-interfaces'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
--- src/topics/02-objects-interfaces.ts
+++ src/topics/02-objects-interfaces.ts
@@ -1,4 +1,20 @@
-let skills : string[] = ["Bash", "Counter", "Healing"];
+const skills : string[] = ["Bash", "Counter", "Healing"];
 
+interface Character{
+    name: string;
+    hp: number,
+    skills: string[];
+    hometown?: string; // el signo define que es optional
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 100,
+    skills: ['Bash', 'Counter'],
+}
+
+strider.hometown = 'Rivendell';
+
+console.table(strider);
 
 export{};
