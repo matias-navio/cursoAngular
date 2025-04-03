f2d0cdd3cb324f9000fdc8eedfb60422232f46f0
Matias Navio <naviomatias2@gmail.com>
2025-04-03 11:38:31 -0300

    Composición de clases

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -3,23 +3,39 @@
 export class Person{
 
     constructor(
-        public name: string, 
+        public firstname?: string,
+        public lastname?: string, 
         public address: string = 'No address'
     ){}
 }
 
 // clase Hero extiende datos y metodos de Person
-export class Hero extends Person{
+// export class Hero extends Person{
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string
+//     ){
+//         super(realName, 'New York ');
+//     }
+// }
+
+// clase Hero que tiena como atributo los datos de Person
+export class Hero{
+
     constructor(
         public alterEgo: string,
         public age: number,
-        public realName: string
+        public realName: string,
+        public person: Person
     ){
-        super(realName, 'New York ');
+        // this.person = new Person(realName, 'New York');
     }
 }
 
-// instancia de la clase Person
-const ironman = new Hero('Ironman', 41, 'Tony');
+// instancia de Person que se usa en Hero
+const tony = new Person('Tony', 'Stark', 'New York')
+// instancia de la clase Hero
+const ironman = new Hero('Ironman', 41, 'Tony', tony);
 
 console.log(ironman)
